6b3aebd9e555b2e10bde8d2712b613293e65409f
Adam(alaplap) <m.dm@gmx.us>
2014-12-23 10:53:46 +0100

    Added responsive design

--- quicksmq.js
+++ quicksmq.js
@@ -17,33 +17,58 @@
    along with this program.  If not, see < http://www.gnu.org/licenses/ >.
 */
 
+/*
+ * Globally visible variables
+ */
 var clientID, activeUserTag;
 
+/*
+ * Setting the client id. Check yours at http://instagram.com/developer/clients/manage/#
+ */ 
 function qsmq_setID(ID) {
    clientID = ID;
 }
-/* YOUR CLIEND ID GOES HERE:*/
-qsmq_setID("59641894f6a24921b037654a1f412253");
 
+/* YOUR CLIEND ID GOES HERE: */
+qsmq_setID("");
 
+/*
+ * Searching all user tags on the page and setting their mouseover event
+ */
+$(document).ready(function() {
+   $('.qsmq-ins-user').each(function() {
+      $(this).mouseover(popUpProfile);
+   });
+});
+
+/*
+ * Ajax API call, searching for the 5 most relevant user by name (`userName` parameter).
+ */
 function searchUser(userName) {
    return $.ajax({
-     	url: "https://api.instagram.com/v1/users/search?q=" + userName + "&client_id=" + clientID + "&callback=?",
+     	url: "https://api.instagram.com/v1/users/search?q=" + userName + "&client_id=" + clientID + "&callback=?&count=5",
       dataType: "json",
       contentType: "application/json; charset=utf-8",
       type: "GET"
    });
 }
 
+/*
+ * Ajax API call, querying the specified user's (`userID` parameter) latest published media.
+ * Doesn't matter if the latest media was a video.
+ */
 function getUserMedia(userID) {
    return $.ajax({
-      url: "https://api.instagram.com/v1/users/" + userID + "/media/recent/?client_id=" + clientID + "&callback=?",
+      url: "https://api.instagram.com/v1/users/" + userID + "/media/recent/?client_id=" + clientID + "&callback=?&count=1",
       dataType: "json",
       contentType: "application/json; charset=utf-8",
       type: "GET"
    });
 }
 
+/*
+ * Ajax API call, querying information about the specified user (`userID` parameter)
+ */
 function getUserInfo(userID) {
    return $.ajax({
       url: "https://api.instagram.com/v1/users/" + userID + "/?client_id=" + clientID + "&callback=?",
@@ -53,53 +78,68 @@ function getUserInfo(userID) {
    });
 }
 
-
+/*
+ * Shortening big numbers
+ */
 function shortNumber(n) {
+   if (n > 999999999)
+      return Math.floor(n/1000000000) + "B +";
    if (n > 999999)
       return Math.floor(n/1000000) + "M +";
-   else if (n > 99999)
+   else if (n > 9999)
       return Math.floor(n/1000) + "K +";
    else
       return n + "";
 }
 
-$(document).ready(function() {
-   $('.qsmq-ins-user').each(function() {
-      $(this).mouseover(popUpProfile);
-   });
-});
+/*
+ * This function is called when an error occures during/after an API call.
+ * It'll change the loading animation color to RED!
+ */
+function displayError(loader) {
+   $(loader).css('background-color', 'red');
+}
 
-
+/*
+ * Making the popup box
+ */
 function popUpProfile() {
-
    var position = $(this).position();
    var height = $(this).height();
    var userName = $(this).text();
    var userID;
+   var docWidth = $(document).width();
    
+   /*
+    * If there is no text inside the tags -> return
+    */
    if($('.qsmq.' + userName).length) return;
    
-   /* popup */
+   /* The "main" `div` of the popup box */
    var popup = document.createElement('div');
    $(popup).addClass('qsmq popup loading ' + userName)
       .css( 'left', position.left )
       .css( 'top', position.top );
          
-      /* popup header */
+      /* Popup box header */
       var header = document.createElement('div');
       $(header).addClass('qsmq-header');
       
+         /* `div` containing the profile picture */
          var profPicDiv = document.createElement('div');
          $(profPicDiv).addClass('prof-pic');
             
+            /* `img` displaying the profile picture */
             var profPic = new Image();
             
+            /* `div` for the loading animation */
             var profLdr = document.createElement('div');
             $(profLdr).addClass('prof-ldr');
             
          $(profPicDiv).append(profPic)
             .append(profLdr);
          
+         /* `a` containing the user name */
          var profName = document.createElement('a');
          $(profName).addClass('prof-name');
          
@@ -108,10 +148,11 @@ function popUpProfile() {
    
    $(popup).append(header);
    
-      /* popup body */
+      /* The "body" `div` of the popup box */
       var body = document.createElement('div');
       $(body).addClass('qsmq-body');
       
+         /* `a` containing the user's website */
          var webLink = document.createElement('a');
          $(webLink).addClass('prof-web');
             
@@ -119,11 +160,13 @@ function popUpProfile() {
       
    $(popup).append(body);
    
-      /* popup footer */
+      /* The "footer" `div` of the popup box */
       var footer = document.createElement('div');
       $(footer).addClass('qsmq-footer');
       
+         /* `div` containing the user's biography */
          var profBio = document.createElement('div');
+         /* `span`s containing the number of posts, followers and followings */
          var profPost = document.createElement('span');
          var profFlwr = document.createElement('span');
          var profFlwi = document.createElement('span');
@@ -138,37 +181,69 @@ function popUpProfile() {
    
    $('body').append( popup );
    
-   /* popup positioning */
+   /* Positioning the popup box */
    $(popup).css('top', position.top + height / 2);
-   /* popup event */
+   
+   /* Adding an event listening to the popup box. Clicking the box will destroy it. */
    $(popup).on("click", function () {
       $(this).remove();
    });
 
+   /* 1. API call, searching the user */
    searchUser(userName).done(function(result) {
+      /* Checking if the API call was successful */
+      if(result.meta.code!=200) {
+         displayError(profLdr);
+         return;
+      }
+      
       userID = result.data[0].id;
       
-      getUserMedia(userID).done(function(result) {         
-         // popup hatter
-         // utolso kep: result.data[0].images.low_resolution.url
+      /* 2. API call, querying the user's latest media */
+      getUserMedia(userID).done(function(result) {
+         /* Checking if the API call was successful */
+         if(result.meta.code!=200) {
+            displayError(profLdr);
+            return;
+         }   
+         
+         // Setting the background image of the popup box
          $(popup).css('background-image', 'url('+result.data[0].images.low_resolution.url+')');
          
-         // profilkep: result.data[0].user.profile_picture
-         profPic.src = result.data[0].user.profile_picture;
+         // Setting the source of the profile picture
+         profPic.src = result.data[0].user.profile_picture;   
          
+         /* Changing the popup box' class to stop the loading animation */
+         $(popup).removeClass().addClass('qsmq popup loaded ' + userName);
+         
+         
+         /* 3. API call, querying information about the user */
          getUserInfo(userID).done(function(result) {
-            // profil nev: result.data.full_name
+            /* Checking if the API call was successful */
+            if(result.meta.code!=200) {
+               displayError(profLdr);
+               return;
+            }   
+         
+            /* 
+             * Settin the content of the `a` and it's `href` attribute, 
+             * containing the user's name and linking to the user's profile page
+             */
             $(profName).append(result.data.full_name);
             $(profName).attr('href', 'http://instagram.com/' + userName )
                .attr('target', '_blank');
                
-            // weboldal link: result.data.website
+            /* 
+             * Settin the content of the `a` and it's `href` attribute, 
+             * containing the user's website link and linking to there
+             */
             var webLinkUrl = result.data.website;
             
             if (webLinkUrl.length !== 0) {
                $(webLink).attr('href', webLinkUrl)
                   .attr('target', '_blank');
                
+               /* 'Shortening the website URL */
                var shortLink = result.data.website;
                shortLink = shortLink.replace('http://','').replace('https://','');
                $(webLink).append(shortLink);
@@ -176,13 +251,14 @@ function popUpProfile() {
                $(body).css('display', 'none');
             }
                   
-            // profil bio: result.data.bio
+            /* Setting the profile biography and shortening it if required */
             var biography = result.data.bio;
             if (biography.length > 150)
                biography = biography.substring(0,150) + "...";
                
             $(profBio).append(biography);
             
+            /* Setting the `span`s containing the number of posts, followers and followings */
             var posts = shortNumber(result.data.counts.media);
             var flwrs = shortNumber(result.data.counts.follows);
             var flwis = shortNumber(result.data.counts.followed_by);               
@@ -191,22 +267,39 @@ function popUpProfile() {
             $(profFlwr).append(flwrs);
             $(profFlwi).append(flwis);
             
-            $(popup).removeClass().addClass('qsmq popup loaded ' + userName);
+            /*
+             * Get width and position data to prevent positioning the
+             * popup box outside of the window
+             */
+            var popupWidth = $(popup).width();
+            var popupPos = $(popup).position();
+           
+            if( (popupPos.left + popupWidth) > docWidth )
+            {
+              $(popup).css( 'left', (docWidth - popupWidth) / 2 );
+            }
             
-            /* popup event */
+            /* 
+             * Adding an event listening to the popup box. 
+             * Leaving the box with the mouse will destroy it. 
+             */
             $(popup).on("mouseleave", function () {
                $(this).remove();
             });
+            
+         /* If the 3. API call fails .. */
          }).fail(function() {
-            $(profLdr).css('background-color', 'red');
+            displayError(profLdr);
          });
          
+      /* If the 2. API call fails .. */
       }).fail(function() {
-         $(profLdr).css('background-color', 'red');
+         displayError(profLdr);
       });
       
+   /* If the 1. API call fails .. */
    }).fail(function() {
-      $(profLdr).css('background-color', 'red');
+      displayError(profLdr);
    });
 
 }
